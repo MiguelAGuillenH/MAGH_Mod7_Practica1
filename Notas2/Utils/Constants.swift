bdf4cce34d52a577f89122e5d2c073ce53408aa9
MAGH <miguel.a.guillen.h@gmail.com>
2024-04-29 18:39:10 -0600

    Changes 2

---
+++ Notas2/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Notas2
+//
+//  Created by MAGH on 25/04/24.
+//
+
+import Foundation
+
+struct Constants {
+    let NOTE_CONTENT_PLACEHOLDER = "Type your note here..."
+}
